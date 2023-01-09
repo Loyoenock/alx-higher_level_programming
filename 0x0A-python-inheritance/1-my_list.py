70f1c8455d905e3557057b70a9bb433ab1709bfb
Loyoenock <loyoenock@gmail.com>
2023-01-09 10:06:03 -0800

    1. My list created

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pythom3
+"""MyList
+"""
+
+
+class MyList(list):
+    """Contans list
+    """
+
+
+    def print_sorted(self):
+        """Prints self in sorted format
+        """
+
+
+        print(sorted(self))
