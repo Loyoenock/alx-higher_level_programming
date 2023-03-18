d83a540458484eb4043c7407cc8f8e2925d5422e
Loyoenock <loyoenock@gmail.com>
2023-03-18 07:03:17 -0700

    3-my_safe_filter_states.py - indention adjusted

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-A list that displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+A list that displays all values in the states table of hbtn_0e_0_usa 
+where name matches the argument.
 """
 import sys
 import MySQLdb
@@ -8,6 +9,7 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                         db=sys.argv[3], port=3306)
+
 
     cur = db.cursor()
     cur.execute("SELECT * \
