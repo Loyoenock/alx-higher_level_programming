f309a60ac231fec0cb68f949d5917332446c3ab4
Loyoenock <loyoenock@gmail.com>
2023-03-31 14:18:27 -0700

    1-hbtn_header.py file added

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A script that takes in a URL, sends a request to it 
+and displays the value of the X-Request-Id variable
+You must use the packages urllib and sys, and with only.
+"""
+if __name__ == "__manin__":
+    import urllib.request as request
+    from sys import argv
+    req = request.Request(argv[1])
+    with request.urlopen(req) as r:
+        print(r.headers.get('X-Request-Id'))
