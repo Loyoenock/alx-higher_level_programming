842ecf086bd15b39860f928c697e31b328534801
Loyoenock <loyoenock@gmail.com>
2023-03-31 15:52:45 -0700

    5-hbtn_header.py - coding style adjusted

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,7 +3,7 @@
 A script that takes in a URL, requests it & displays 
 variable X-Request-Id value in the response header
 """
-if __name__ == "__main__":
+if __name__ == '__main__':
     import requests
     from sys import argv
     r = requests.get(argv[1])
