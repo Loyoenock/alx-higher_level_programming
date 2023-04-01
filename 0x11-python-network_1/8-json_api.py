b6c8be4339d6c5239891df63832104fcfb3844b8
Loyoenock <loyoenock@gmail.com>
2023-03-31 17:09:35 -0700

    8-json_api.py style check

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
         q = argv[1]
     else:
         q = ''
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
-    try:
-        r_dict = r.json()
-        name = r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
-            print("No result")
-        else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    except:
-        print("Not a valid JSON")
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        try:
+            r_dict = r.json()
+            name = r_dict.get('name')
+            if len(r_dict) == 0 or not id or not name:
+                print("No result")
+            else:
+                print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+        except:
+            print("Not a valid JSON")
