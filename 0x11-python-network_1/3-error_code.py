7881f68a79eb9e72859de845e631a3cad117c92b
Loyoenock <loyoenock@gmail.com>
2023-03-31 17:05:06 -0700

    3-error_code.py - style check

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A script that takes in a URL, sends it a request 
-& displays the body of the response (decoded in utf-8).
+A script that takes in a URL, sends it a request
+& displays the body of the response (decoded in utf-8)
 """
 if __name__ == "__main__":
     import urllib.error as error
